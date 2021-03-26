23a064de6e784a7d0b5ee86923737143ec9deeba
longshenzhu <1223006618@qq.com>
2021-03-26 18:14:56 +0800

    save

---
+++ vue/my-qingfeng-mall/src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <HelloWorld msg="Welcome to about"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  name: 'About',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
